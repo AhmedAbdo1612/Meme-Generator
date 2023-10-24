cbbc511fe9afa16c782d659ff725c5048c03f115
Ahmed <ahmaadaabdo@gmail.com>
2023-10-24 03:34:21 +0300

    Updates

---
+++ precache-manifest.aa37ebb44b8c5fee06c9d4cb92a3c632.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac0bc532622100dc2906c17d6b882cd5",
+    "url": "/Meme-Generator/index.html"
+  },
+  {
+    "revision": "7972923bb3c1dc65ddc7",
+    "url": "/Meme-Generator/static/css/2.34507a92.chunk.css"
+  },
+  {
+    "revision": "fc5519be2e828dcfaaec",
+    "url": "/Meme-Generator/static/css/main.8572f5b2.chunk.css"
+  },
+  {
+    "revision": "7972923bb3c1dc65ddc7",
+    "url": "/Meme-Generator/static/js/2.9205716c.chunk.js"
+  },
+  {
+    "revision": "fc5519be2e828dcfaaec",
+    "url": "/Meme-Generator/static/js/main.521299ca.chunk.js"
+  },
+  {
+    "revision": "a1b3edea92dd6eed2d90",
+    "url": "/Meme-Generator/static/js/runtime~main.730703d4.js"
+  }
+]);
